4f8b76ccccaff31b37f84a53d4525a2215ff98cf
Yijia Diao <LittleQili@users.noreply.github.com>
2021-05-28 17:23:08 +0800

    learn shader io lang

--- opengl/hw2/hw2/gltriangle.cpp
+++ opengl/hw2/hw2/gltriangle.cpp
@@ -20,15 +20,18 @@ const unsigned int SCR_HEIGHT = 600;
 // 这里的in是指input
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 const char *fragmentShaderSource1 = "#version 330 core\n"
+    "in vec4 vertexColor;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vertexColor;\n"
     "}\n\0";
 const char *fragmentShaderSource2 = "#version 330 core\n"
     "out vec4 FragColor;\n"
